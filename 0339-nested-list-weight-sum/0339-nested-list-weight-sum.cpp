c99b8b906924afd6d14af328b2e0e4b1b1e49e5e
Huy Phung <huyphung3103@gmail.com>
2025-11-01 21:06:48 -0400

    Time: 0 ms (100%), Space: 13.3 MB (42.21%) - LeetHub

---
+++ 0339-nested-list-weight-sum/0339-nested-list-weight-sum.cpp
@@ -0,0 +1,64 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than
+ * a nested list. bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a
+ * single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer
+ * to it. void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a
+ * nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    int depthSum(vector<NestedInteger>& nestedList) {
+        int depth = 1;
+        int sum = 0;
+        queue<NestedInteger> q;
+        for (auto n : nestedList) {
+            if (n.isInteger()) {
+                sum = sum + depth * n.getInteger();
+            } else {
+                for (auto x : n.getList()) {
+                    q.push(x);
+                }
+            }
+        }
+        while (!q.empty()) {
+            depth++;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                NestedInteger cur = q.front();
+                q.pop();
+                if (cur.isInteger()) {
+                    sum = sum + depth * cur.getInteger();
+                } else {
+                    for (auto x : cur.getList()) {
+                        q.push(x);
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+};
