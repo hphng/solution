154939762790bc078718e531a1b8a423c4a1d260
Huy Phung <huyphung3103@gmail.com>
2025-12-03 22:23:59 -0500

    Time: 0 ms (100%), Space: 19.3 MB (64.06%) - LeetHub

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countCollisions(string directions) {
+        int n = directions.length();
+        int l = 0, r = n - 1;
+        while(l < n && directions[l] == 'L'){
+            l++;
+        }
+
+        while(r >= 0 && directions[r] == 'R'){
+            r--;
+        }
+        int ans = 0;
+        for(int i = l; i <= r; i++){
+            ans = ans + (directions[i] != 'S');
+        }
+        return ans;
+    }
+};
