a6790842557f651853f6901873f17328e7416b1c
Huy Phung <huyphung3103@gmail.com>
2026-03-06 00:48:29 -0500

    Time: 0 ms (100%), Space: 15.2 MB (34.69%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size()-1;
-        int mid;
+        int left = 0, right = nums.size() - 1;
         while(left <= right){
-            mid = (left+right)/2;
-            if(nums[mid] == target){
+            int mid = (left + right)/2;
+            if(nums[mid] == target) {
                 return mid;
             }
-            if(nums[left] <= nums[mid]){
-                if(nums[left] <= target && target < nums[mid]){
+
+            bool sorted = nums[left] <= nums[mid];
+            if(sorted) {
+                if(nums[left] <= target && target < nums[mid]) {
                     right = mid -1;
                 } else {
                     left = mid + 1;
@@ -18,10 +19,9 @@ public:
                 if(nums[mid] < target && target <= nums[right]){
                     left = mid + 1;
                 } else {
-                    right = mid - 1;
+                    right = mid -1;
                 }
             }
-            // cout << mid << endl;
         }
         return -1;
     }
