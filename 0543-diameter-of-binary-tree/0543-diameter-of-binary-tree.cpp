e8a1876154f21d37901d95b8db025c11d49b6b61
Huy Phung <huyphung3103@gmail.com>
2026-03-06 23:16:38 -0500

    Time: 0 ms (100%), Space: 23.8 MB (19.01%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,17 +11,19 @@
  */
 class Solution {
 public:
-    int dfs(TreeNode* root, int& maxDistance){
-        if(!root) return 0; 
-        int left = dfs(root -> left, maxDistance);
-        int right = dfs(root -> right, maxDistance);
-        // cout << root -> val << " " << left << " " << right << endl;
-        maxDistance = max(maxDistance, left + right);
+    int ans = 0;
+    int dfs(TreeNode* root) {
+        if(!root) return 0;
+        if(!root -> left && !root -> right) {
+            return 1;
+        }
+        int left = dfs(root -> left);
+        int right = dfs(root -> right);
+        ans = max(ans, left + right);
         return max(left, right) + 1;
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        int ans = 0;
-        dfs(root, ans);
+        dfs(root);
         return ans;
     }
 };
