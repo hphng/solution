d86259488c33f35476c3bc2d5fcf45c20c28ad44
Huy Phung <huyphung3103@gmail.com>
2026-03-25 13:51:28 -0400

    Time: 0 ms (100%), Space: 8.3 MB (21.7%) - LeetHub

---
+++ 0276-paint-fence/0276-paint-fence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numWays(int n, int k) {
+        vector<int> dp(n+1);
+
+        if(n == 0){
+            return 0;
+        }
+
+        if(n == 1){
+            return k;
+        }
+
+        dp[0] = 0;
+        dp[1] = k;
+        dp[2] = k*k;
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i-1] + dp[i-2])* (k-1);
+        }
+
+        return dp[n];
+    }
+};
